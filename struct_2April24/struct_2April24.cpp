f11b600b93cefaf432c5e1f0bd3a965b796093d8
dianfitrip <dianfitri683@gmail.com>
2024-04-02 10:15:35 +0700

    lanjutan struct nya

--- struct_2April24/struct_2April24.cpp
+++ struct_2April24/struct_2April24.cpp
@@ -22,5 +22,9 @@ int main()
 	cout << "Umur Mahasiswa : ";
 	cin >> mhs.umur;
 
-	
+	cout << endl;
+	cout << "\n Nim : " << mhs.nim; 
+	cout << "\n Nama : " << mhs.nama; 
+	cout << "\n Alamat : " << mhs.alamat; 
+	cout << "\n Umur : " << mhs.umur; 
 }
