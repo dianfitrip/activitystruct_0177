a80b00e91ee27546285c5600b949b84ab44ec1a4
dianfitrip <dianfitri683@gmail.com>
2024-04-02 10:30:27 +0700

    detai alamat untuk desa

--- struct part2/struct part2.cpp
+++ struct part2/struct part2.cpp
@@ -10,6 +10,29 @@ struct Mahasiswa
 {
 	string nim;
 	string nama;
-	string alamat;
+	DetailAlamat alamat;
 	string umur;
 };
+
+int main()
+{
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+
+	cout << "Alamat Mahasiswa : ";
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.desa;
+	
+
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+}
